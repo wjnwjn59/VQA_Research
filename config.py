3dba62ded0e05a56cba4e0b6d02262c51c0a1d24
wjnwjn59 <thangduong.no01@gmail.com>
2024-09-15 14:35:13 +0700

    update config for model training

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class PipelineConfig:
         self.seed = 59
         self.learning_rate = 1e-5
         self.epochs = 30
-        self.train_batch_size = 8
+        self.train_batch_size = 32
         self.test_batch_size = 64
         self.hidden_dim = 2048
         self.projection_dim = 2048
@@ -14,9 +14,11 @@ class PipelineConfig:
         self.text_encoder_id = "vinai/bartpho-word"
         self.img_encoder_id = "timm/resnet18.a1_in1k"
         self.paraphraser_id = "chieunq/vietnamese-sentence-paraphase"
-        self.num_paraphrase = 2
-        self.paraphrase_thresh = 0.9
-        self.val_set_ratio = 0.1
+        self.is_text_augment = True
+        self.num_paraphrase = 1
+        self.paraphrase_thresh = 0.6
+        self.n_para_pool = 10
+        self.is_img_augment = False
         self.data_dir = "/home/VLAI/datasets"
 
 pipeline_config = PipelineConfig()
