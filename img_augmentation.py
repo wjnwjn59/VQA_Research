17b1f4188cc56d903ec733c1212bda4d3a2867b3
wjnwjn59 <thangduong.no01@gmail.com>
2024-09-18 01:34:52 +0700

    add augmentation func for image channel

---
+++ img_augmentation.py
@@ -0,0 +1,14 @@
+from torchvision import transforms
+
+def augment_image(img_pil, n_img_augmentations=2):
+    augmented_imgs = [] 
+    transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(p=1),
+        transforms.RandomVerticalFlip(p=1),
+        transforms.RandomRotation(degrees=90)
+    ])
+    for _ in range(n_img_augmentations):
+        augmented_img = transform(img_pil)
+        augmented_imgs.append(augmented_img)
+
+    return augmented_imgs
