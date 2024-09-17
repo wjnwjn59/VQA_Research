aeb52672ba871ffc95e23f2dcba32561248e65b3
wjnwjn59 <thangduong.no01@gmail.com>
2024-09-18 01:35:06 +0700

    add load img model func

--- img_encoder.py
+++ img_encoder.py
@@ -1,18 +1,21 @@
 import timm
 import torch
 
-from config import pipeline_config
-
-IMG_MODEL_ID = pipeline_config.img_encoder_id
-
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-# get model specific transforms (normalization, resize)
-img_model = timm.create_model(
-    IMG_MODEL_ID,
-    pretrained=True,
-    num_classes=0 # remove classifier nn.Linear
-).to(device)
-data_config = timm.data.resolve_model_data_config(img_model)
-img_processor = timm.data.create_transform(**data_config, 
-                                           is_training=False)
+def load_img_encoder(img_encoder_id):
+    img_model = timm.create_model(
+        img_encoder_id,
+        pretrained=True,
+        num_classes=0 # remove classifier nn.Linear
+    ).to(device)
+    data_config = timm.data.resolve_model_data_config(img_model)
+    img_processor = timm.data.create_transform(**data_config, 
+                                               is_training=False)
+
+    return {
+        'model_name': img_encoder_id,
+        'img_processor': img_processor,
+        'latent_space': img_model.num_features * 7 * 7,
+        'img_model': img_model
+    }
