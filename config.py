b80049d1a836a33dcf6154d0bbdb1126c4066b5c
wjnwjn59 <thangduong.no01@gmail.com>
2024-09-22 23:54:37 +0700

    add new config

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class PipelineConfig:
     def __init__(self):
-        self.seed = 60
+        self.seed = 59
         self.learning_rate = 1e-5
         self.epochs = 30
         self.train_batch_size = 32
@@ -16,11 +16,13 @@ class PipelineConfig:
         self.paraphraser_id = "chieunq/vietnamese-sentence-paraphase"
         self.is_text_augment = True
         self.n_text_paras = 1
-        self.text_para_thresh = 0.8
-        self.n_text_para_pool = 10
+        self.text_para_thresh = 0.6
+        self.n_text_para_pool = 20
         self.is_img_augment = False
         self.n_img_augments = 1
-        self.img_augment_thresh = 0.4
+        self.img_augment_thresh = 1.0
+        self.use_dynamic_thresh = False
+        self.dataset_name = 'vivqa'
         self.data_dir = "/home/VLAI/datasets"
 
 pipeline_config = PipelineConfig()
