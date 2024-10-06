b38e41d02e6d0ff8cd2cf3aad505960c9023452d
wjnwjn59 <thangduong.no01@gmail.com>
2024-10-06 10:46:13 +0700

    modify train file to adapt with new logic

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-os.environ["WORLD_SIZE"] = '1'
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '0,1'
+os.environ["WORLD_SIZE"] = '2'
 
 from dotenv import load_dotenv
 
@@ -42,17 +43,31 @@ def set_seed(random_seed):
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
     os.environ['CUDNN_DETERMINISTIC'] = '1'
 
-def save_model(save_path, model):
+def set_threads(n_threads, is_print_available_threads=False):
+    if is_print_available_threads:
+        num_threads = torch.get_num_threads()
+        print(f"Current number of threads: {num_threads}")
+    torch.set_num_threads(n_threads)
+    torch.set_num_interop_threads(n_threads)
+
+def save_model(save_path, model, optimizer, scaler):
     try:
-        state_dict = model.module.state_dict()
+        model_state_dict = model.module.state_dict()
     except AttributeError:
-        state_dict = model.state_dict()
+        model_state_dict = model.state_dict()
 
-    torch.save(state_dict, save_path)
+    checkpoint = {
+        'model': model_state_dict,
+        'optimizer': optimizer.state_dict(),
+        'scaler': scaler.state_dict()
+    }
 
-def free_vram(model, optimizer):
+    torch.save(checkpoint, save_path)
+
+def free_vram(model, optimizer, scaler):
     del model
     del optimizer
+    del scaler
     torch.cuda.empty_cache()
 
 def compute_accuracy(logits, labels):
@@ -69,10 +84,16 @@ def evaluate(model, val_loader, criterion):
     with torch.no_grad():
         for idx, batch in enumerate(val_loader):
             text_inputs_lst = batch.pop('text_inputs_lst')
-            img_inputs = batch.pop('img_inputs')
+            img_inputs_lst = batch.pop('img_inputs')
             labels = batch.pop('labels')
 
-            logits = model(text_inputs_lst, img_inputs)
+            text_inputs_lst = [
+                {k: v.squeeze().to(device, non_blocking=True) for k, v in input_ids.items()} \
+                    for input_ids in text_inputs_lst]
+            img_inputs_lst = [inputs.to(device, non_blocking=True) for inputs in img_inputs_lst]
+            labels = labels.to(device, non_blocking=True)
+
+            logits = model(text_inputs_lst, img_inputs_lst)
 
             loss = criterion(logits, labels)
             acc = compute_accuracy(logits, labels)
@@ -92,9 +113,12 @@ def train(model,
           epochs, 
           criterion, 
           optimizer, 
+          scaler,
           patience=5,
           save_best_path='./weights/best.pt',
-          is_log_result=True):
+          is_log_result=True,
+          is_multi_gpus=True,
+          use_amp=True):
     
     best_val_loss = np.inf
     epochs_no_improve = 0
@@ -103,8 +127,12 @@ def train(model,
     train_acc_lst = []
     val_loss_lst = []
     val_acc_lst = []
+
     for epoch in range(epochs):
-        model.update_epoch(epoch)
+        if is_multi_gpus:
+            model.module.update_epoch(epoch)
+        else:
+            model.update_epoch(epoch)
         train_batch_loss_lst = []
         train_batch_acc_lst = []
 
@@ -114,21 +142,28 @@ def train(model,
         model.train()
         for batch in epoch_iterator:
             text_inputs_lst = batch.pop('text_inputs_lst')
-            img_inputs = batch.pop('img_inputs')
+            img_inputs_lst = batch.pop('img_inputs')
             labels = batch.pop('labels')
+            
+            text_inputs_lst = [
+                {k: v.squeeze().to(device, non_blocking=True) for k, v in input_ids.items()} \
+                    for input_ids in text_inputs_lst]
+            img_inputs_lst = [inputs.to(device, non_blocking=True) for inputs in img_inputs_lst]
+            labels = labels.to(device, non_blocking=True)
 
-            logits = model(text_inputs_lst, img_inputs)
+            with torch.autocast(device_type=device, dtype=torch.float16, enabled=use_amp):
+                logits = model(text_inputs_lst, img_inputs_lst)
+                loss = criterion(logits, labels)
 
-            loss = criterion(logits, labels)
             acc = compute_accuracy(logits, labels)
 
             train_batch_loss_lst.append(loss.item())
             train_batch_acc_lst.append(acc)
 
-            loss.backward()
-
-            optimizer.step()
-            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+            optimizer.zero_grad() 
 
             epoch_iterator.set_postfix({'Batch Loss': loss.item()})
 
@@ -158,7 +193,7 @@ def train(model,
         if val_loss < best_val_loss:
             best_val_loss = val_loss
             epochs_no_improve = 0
-            save_model(save_best_path, model)
+            save_model(save_best_path, model, optimizer, scaler)
         else:
             epochs_no_improve += 1
             if epochs_no_improve >= patience:
@@ -170,7 +205,7 @@ def train(model,
 def parse_args():
     parser = argparse.ArgumentParser(description='ViVQA Training Script')
     parser.add_argument('--seed', type=int, default=pipeline_config.seed, help='Random seed')
-    parser.add_argument('--gpus', type=str, default='0', help='Number of GPUs used')
+    parser.add_argument('--gpus', type=str, default='0,1', help='Number of GPUs used')
     parser.add_argument('--project_name', type=str, default='vivqa_paraphrase_augmentation', help='Project name for wandb')
     parser.add_argument('--exp_name', type=str, help='Experiment name for wandb')
     parser.add_argument('--dataset_name', default=pipeline_config.dataset_name, type=str, help='Name of the dataset')
@@ -195,6 +230,7 @@ def parse_args():
     parser.add_argument('--use_dynamic_thresh', type=lambda x: (str(x).lower() == 'true'), default=pipeline_config.use_dynamic_thresh, help='Use dynamic threshold scaled by epochs')
     parser.add_argument('--save_ckpt_dir', type=str, default='runs/train', help='Directory to save checkpoints')
     parser.add_argument('--is_log_result', type=lambda x: (str(x).lower() == 'true'), default=True, help='Log training and eval results to wandb')
+    parser.add_argument('--use_amp', type=lambda x: (str(x).lower() == 'true'), default=True, help='Use mixed precision training')
     
     return parser.parse_args()
 
@@ -202,6 +238,7 @@ def main():
     args = parse_args()
 
     set_seed(args.seed)
+    set_threads(4)
 
     if args.text_encoder_id:
         text_encoder_dict = load_text_encoder(args.text_encoder_id)
@@ -228,9 +265,12 @@ def main():
                        text_para_thresh=args.text_para_thresh,
                        img_augment_thresh=args.img_augment_thresh)
     
-    if len(args.gpus.split(',')) > 1:
+    is_multi_gpus = len(args.gpus.split(',')) > 1
+    if is_multi_gpus:
         model = nn.DataParallel(model,
                                 device_ids=list(map(int, args.gpus.split(','))))
+
+    model = torch.compile(model, mode='default') 
     model = model.to(device)
         
     optimizer = torch.optim.AdamW(model.parameters(),
@@ -238,8 +278,8 @@ def main():
                                   weight_decay=args.weight_decay)
 
     criterion = nn.CrossEntropyLoss()
+    scaler = torch.amp.GradScaler(enabled=args.use_amp)
 
-    # In your main function:
     train_dataset = get_dataset(text_encoder_dict=text_encoder_dict,
                                 img_encoder_dict=img_encoder_dict,
                                 label_encoder=label2idx,
@@ -251,20 +291,25 @@ def main():
                                label_encoder=label2idx,
                                is_train=False, 
                                **vars(args))
-    
 
     train_loader = DataLoader(train_dataset,
                               batch_size=args.train_batch_size,
+                              pin_memory=True,
+                            #   num_workers=1,
+                            #   multiprocessing_context='spawn',
                               shuffle=True)
 
     test_loader = DataLoader(test_dataset,
                              batch_size=args.test_batch_size,
+                             pin_memory=True,
                              shuffle=False)
 
     if not args.exp_name:
-        text_augment_info = f'istextaug{args.is_text_augment}_ntextpara{args.n_text_paras}_random{args.text_para_thresh}_nparapool{args.n_text_para_pool}'
-        img_augment_info = f'isimgaug{args.is_img_augment}_nimgaug{args.n_img_augments}_random{args.img_augment_thresh}'
-        exp_name = f'phase4_seed{args.seed}_{args.dataset_name}_{text_augment_info}_{img_augment_info}'
+        # text_augment_info = f'istextaug{args.is_text_augment}_ntextpara{args.n_text_paras}_random{args.text_para_thresh}_nparapool{args.n_text_para_pool}'
+        # img_augment_info = f'isimgaug{args.is_img_augment}_nimgaug{args.n_img_augments}_random{args.img_augment_thresh}'
+        text_augment_info = f'istextaug{args.is_text_augment}'
+        img_augment_info = f'isimgaug{args.is_img_augment}'
+        exp_name = f'phase6_seed{args.seed}_{args.dataset_name}_curr{args.use_dynamic_thresh}&0.6_{text_augment_info}_{img_augment_info}'
     else:
         exp_name = args.exp_name
 
@@ -287,11 +332,14 @@ def main():
                                                                      epochs=args.epochs, 
                                                                      criterion=criterion, 
                                                                      optimizer=optimizer, 
+                                                                     scaler=scaler,
                                                                      patience=args.patience,
                                                                      save_best_path=save_best_path,
-                                                                     is_log_result=args.is_log_result)
+                                                                     is_log_result=args.is_log_result,
+                                                                     is_multi_gpus=is_multi_gpus,
+                                                                     use_amp=args.use_amp)
     
-    free_vram(model, optimizer)
+    free_vram(model, optimizer, scaler)
 
     best_model = ViVQAModel(projection_dim=args.projection_dim,
                             hidden_dim=args.hidden_dim,
@@ -301,7 +349,12 @@ def main():
                             is_text_augment=False, 
                             is_img_augment=False).to(device)
 
-    best_model.load_state_dict(torch.load(save_best_path))
+    dev = torch.cuda.current_device()
+    checkpoint = torch.load(save_best_path,
+                            weights_only=True,
+                            map_location = lambda storage, loc: storage.cuda(dev))
+
+    best_model.load_state_dict(checkpoint['model'])
 
     test_loss, test_acc = evaluate(model=best_model,
                                    val_loader=test_loader,
