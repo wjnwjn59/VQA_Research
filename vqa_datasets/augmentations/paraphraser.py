f7177fc97b05a260958b6bc67978632f6d266af4
Koii2k3 <devkoi2k3@gmail.com>
2024-11-03 00:21:01 +0700

    update gen dataset w filter option

--- vqa_datasets/augmentations/paraphraser.py
+++ vqa_datasets/augmentations/paraphraser.py
@@ -2,6 +2,13 @@ import torch
 from transformers import MT5Tokenizer, MT5ForConditionalGeneration
 from config import pipeline_config
 
+# SBert
+from sentence_transformers import SentenceTransformer, util
+# KNN
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 PARAPHRASER_ID = pipeline_config.paraphraser_id
@@ -29,3 +36,38 @@ def get_paraphrase(text, num_return_sequences):
         paraphrase_lst.append(paraphraser_tokenizer.decode(beam_output, skip_special_tokens=True))
 
     return paraphrase_lst
+
+
+def knn_filter(origin, para_lst, from_index, ktop):
+    collection = [origin] + para_lst
+    
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(collection)
+    
+    scores = cosine_similarity(tfidf_matrix[:1], tfidf_matrix[1:]).flatten()
+    
+    paraphrase_with_scores = list(zip(para_lst, scores))
+    paraphrase_with_scores_sorted = sorted(paraphrase_with_scores, key=lambda x: x[1], reverse=True)
+    
+    paraphrase_with_scores_sorted = [para for para, score in paraphrase_with_scores_sorted]
+    filtered_para_questions = paraphrase_with_scores_sorted[from_index:(from_index + ktop)]
+    
+    return filtered_para_questions
+
+
+def sbert_filter(origin, para_lst, from_index, ktop):
+    collection = [origin] + para_lst
+    
+    model = SentenceTransformer('paraphrase-MiniLM-L6-v2')
+    paraphrase_with_scores = []
+    
+    for para in para_lst:
+        score = util.cos_sim(model.encode(origin), model.encode(para)).item()
+        paraphrase_with_scores.append((para, score))
+        
+    paraphrase_with_scores_sorted = sorted(paraphrase_with_scores, key=lambda x: x[1], reverse=True)
+    paraphrase_with_scores_sorted = [para for para, score in paraphrase_with_scores_sorted]
+    filtered_para_questions = paraphrase_with_scores_sorted[from_index:(from_index + ktop)]
+    
+    return filtered_para_questions
+    
