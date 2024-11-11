403d6301c4d695acf828a50a68c1cedb47fba1ec
Koii2k3 <devkoi2k3@gmail.com>
2024-11-12 00:41:15 +0700

    Add CrossAttention & rm AugImg

--- lightweight_vqa_model.py
+++ lightweight_vqa_model.py
@@ -1,36 +1,37 @@
 import torch
 import torch.nn as nn
-
+import numpy as np
 from torch.functional import F
+from cross_attention import *
+
 
 class BottleneckBlock(nn.Module):
     def __init__(self, input_dim):
         super().__init__()
         self.proj = nn.Sequential(
-            nn.Linear(input_dim, input_dim // 2),  # Bottleneck layer
-            nn.ReLU(),  # Non-linearity (ReLU for lightweight performance)
-            nn.Linear(input_dim // 2, input_dim),  # Expand back to original dimension
+            nn.Linear(input_dim, input_dim // 2),
+            nn.ReLU(),
+            nn.Linear(input_dim // 2, input_dim),
         )
-        self.norm = nn.LayerNorm(input_dim)
+        # self.norm = nn.LayerNorm(input_dim)
 
     def forward(self, x):
         x = self.proj(x) + x
-        x = self.norm(x)
-        return x 
+        # x = self.norm(x)
+        return x
 
 
 # Define a Text Encoder class that handles the text input and projects it into a new dimension.
 class TextEncoder(nn.Module):
     def __init__(self, text_model, projection_dim, is_text_augment):
         super().__init__()
-        
+
         # Enable gradient updates for the text model
         for param in text_model.parameters():
             param.requires_grad = True
-            
+
         self.is_text_augment = is_text_augment  # Flag for augmenting text data
         self.model = text_model  # Text model
-        # self.norm = nn.LayerNorm(projection_dim)
 
         self.proj = nn.Sequential(
             nn.Linear(self.model.config.hidden_size, projection_dim),
@@ -38,75 +39,79 @@ class TextEncoder(nn.Module):
         )
 
         # Bottleneck structure for augment_linear
+        # if self.is_text_augment:
+        #     self.augment_linear = BottleneckBlock(projection_dim)
+
         if self.is_text_augment:
-            self.augment_linear = BottleneckBlock(projection_dim)
+            config = LanguageConfig()
+            self.language_query_attn = CrossAttention(
+                encoder_hidden_size=config.hidden_size,
+                num_heads=config.self_attn_heads,
+                hidden_size=config.hidden_size,
+                dropout=config.attn_dropout
+            )
+
+            self.lang_cross_augment = CrossAugmentation(
+                attn_layer=self.language_query_attn,
+                mlp_layer=MLP(config),
+                norm_layer=nn.LayerNorm(
+                    config.hidden_size, eps=config.layer_norm_eps)
+            )
 
     def forward(self, text_inputs_lst, augment_thresh):
-        r = torch.rand(1)  # Generate a random value to decide if augmentation should be applied
+        r = torch.rand(1)
         if self.training and self.is_text_augment and r < augment_thresh:
+
             embed_lst = []
             for text_inputs in text_inputs_lst:
-                x = self.model(**text_inputs)  # Forward pass through the text model
-                x = x['last_hidden_state'][:, 0, :]  # Extract the embedding of the [CLS] token
+                x = self.model(**text_inputs)
+                # x = x['last_hidden_state'][:, 0, :]
+
+                x = x['last_hidden_state']
                 embed_lst.append(x)
 
-            # Stack embeddings and sum them for augmented inputs
-            para_features_t = torch.stack(embed_lst, dim=1)
-            x = torch.sum(para_features_t, dim=1)  # Sum the embeddings along the new dimension
-            x = self.proj(x) 
-            x = self.augment_linear(x) 
+            # para_features_t = torch.stack(embed_lst, dim=1)
+            # Sum the embeddings along the new dimension
+            # x = torch.sum(para_features_t, dim=1)
+            # x = self.proj(x)
+            # x = self.augment_linear(x)
+
+            ori_embed = embed_lst[0]
+            para_embed = embed_lst[1:]
+
+            for _ in range(1):
+                ori_embed = self.lang_cross_augment(ori_embed, para_embed)
+
+            pooled_output = ori_embed[:, 0, :]
+            x = self.proj(pooled_output)
 
         else:
-            # Process a single text input if no augmentation is applied
             text_inputs = text_inputs_lst[0]
-            x = self.model(**text_inputs)  # Forward pass through the text model
-            x = x['last_hidden_state'][:, 0, :]  # Extract the embedding of the [CLS] token
-            x = self.proj(x) 
+            x = self.model(**text_inputs)
+            x = x['last_hidden_state'][:, 0, :]
+            x = self.proj(x)
 
         return x
 
 
-# Define an Image Encoder class that handles the image input and projects it into a new dimension.
 class ImageEncoder(nn.Module):
-    def __init__(self, img_model, projection_dim, is_img_augment):
+    def __init__(self, img_model, projection_dim):
         super().__init__()
-        
-        # Enable gradient updates for the image model
+
         for param in img_model.parameters():
             param.requires_grad = True
-        self.is_img_augment = is_img_augment  # Flag for image augmentation
-        self.model = img_model  # Image model
 
-        # Linear projection for flattened features
+        self.model = img_model
+
         self.proj = nn.Sequential(
             nn.Linear(self.model.num_features * 7 * 7, projection_dim),
             nn.ReLU()
         )
 
-        # Bottleneck structure for 
-        if self.is_img_augment:
-            self.augment_linear = BottleneckBlock(projection_dim)
-
-    def forward(self, img_inputs_lst, augment_thresh):
-        r = torch.rand(1)  # Random value to decide if augmentation is applied
-        if self.training and self.is_img_augment and r < augment_thresh:
-            embed_lst = [] 
-            for img_inputs in img_inputs_lst:
-                x = self.model.forward_features(img_inputs)  # Extract features from the image model
-                x = x.view(x.size(0), -1)  # Flatten the feature maps
-                embed_lst.append(x)
-
-            # Stack and sum embeddings for augmented inputs
-            img_features_t = torch.stack(embed_lst, dim=1)
-            x = torch.sum(img_features_t, dim=1)
-            x = self.proj(x)
-            x = self.augment_linear(x)  # Apply the bottleneck structure
-        else: 
-            # Process a single image input if no augmentation is applied
-            x = self.model.forward_features(img_inputs_lst[0])
-            x = x.view(x.size(0), -1)
-            x = self.proj(x)       
-
+    def forward(self, img_inputs_lst):
+        x = self.model.forward_features(img_inputs_lst[0])
+        x = x.view(x.size(0), -1)
+        x = self.proj(x)
         return x
 
 
@@ -114,11 +119,11 @@ class Classifier(nn.Module):
     def __init__(self, projection_dim, hidden_dim, answer_space):
         super().__init__()
         self.fc = nn.Linear(projection_dim * 2, hidden_dim)
-        self.dropout = nn.Dropout(0.2)  
-        self.classifier = nn.Linear(hidden_dim, answer_space)  # Final classification layer
+        self.dropout = nn.Dropout(0.2)
+        self.classifier = nn.Linear(hidden_dim, answer_space)
 
     def forward(self, text_f, img_f):
-        x = torch.cat((img_f, text_f), 1)  # Concatenate text and image features
+        x = torch.cat((img_f, text_f), 1)
         x = self.fc(x)
         x = F.relu(x)
         x = self.dropout(x)
@@ -129,58 +134,109 @@ class Classifier(nn.Module):
 
 # Main model class combining text, image encoders, and classifier for VQA (Visual Question Answering)
 class ViVQAModel(nn.Module):
-    def __init__(self, projection_dim, hidden_dim, answer_space_len, 
+    def __init__(self, projection_dim, hidden_dim, answer_space_len,
                  text_encoder_dict, img_encoder_dict,
-                 is_text_augment=True, is_img_augment=False,
-                 total_epochs=100, use_dynamic_thresh=True, 
-                 text_para_thresh=0.6, img_augment_thresh=0.6):
-        
+                 is_text_augment=True,
+                 total_epochs=100, use_dynamic_thresh=True,
+                 start_threshold=0.6, update_threshold_method='linear', init_learning_rate=1e-5,
+                 text_para_thresh=0.6):
+
         super().__init__()
-        
-        # Initialize the text encoder
+
         self.text_encoder = TextEncoder(text_model=text_encoder_dict['text_model'],
                                         projection_dim=projection_dim,
                                         is_text_augment=is_text_augment)
-        
-        # Initialize the image encoder        
-        self.img_encoder = ImageEncoder(img_model=img_encoder_dict['img_model'],
-                                        projection_dim=projection_dim,
-                                        is_img_augment=is_img_augment)
 
-        # Initialize the classifier        
+        self.img_encoder = ImageEncoder(img_model=img_encoder_dict['img_model'],
+                                        projection_dim=projection_dim)
+
         self.classifier = Classifier(projection_dim=projection_dim,
                                      hidden_dim=hidden_dim,
                                      answer_space=answer_space_len)
-    
-        # Dynamic threshold settings for augmentation
+
         self.use_dynamic_thresh = use_dynamic_thresh
         self.text_para_thresh = text_para_thresh
-        self.img_augment_thresh = img_augment_thresh
         self.total_epochs = total_epochs
+        self.first_loss = 0
         self.current_epoch = 0
-        self.start_threshold = 0.6
-        self.min_threshold = 0.0
+        self.update_threshold_method = update_threshold_method
+        self.start_threshold = start_threshold
+        self.min_threshold = 0.01
 
-    # Compute dynamic thresholds for augmentation based on the current epoch
+        self.init_learning_rate = init_learning_rate
+
     def get_threshold(self):
         if not self.use_dynamic_thresh:
-            return self.text_para_thresh, self.img_augment_thresh
-        
-        decay = (self.start_threshold - self.min_threshold) * (self.current_epoch / self.total_epochs)
-        updated_thresh = max(self.start_threshold - decay, self.min_threshold)
+            return self.text_para_thresh
 
-        return updated_thresh, updated_thresh
+        if self.update_threshold_method == 'linear':
+            # Linear threshold
+            decay = (self.start_threshold - self.min_threshold) * \
+                (self.current_epoch / self.total_epochs)  # 0.6 * 1/30
+            updated_thresh = max(self.start_threshold -
+                                 decay, self.min_threshold)
 
-    # Forward pass for the entire model (combines text and image inputs)
+        elif self.update_threshold_method == 'exponential_epoch':
+            # Exponent threshold base on Epoch
+            k = (-1/self.total_epochs) * \
+                np.log(self.min_threshold/self.start_threshold)
+            k /= 3
+            updated_thresh = max(
+                self.start_threshold * np.exp(-k * self.current_epoch), self.min_threshold)
+
+        elif self.update_threshold_method == 'exponential_loss':
+            if self.current_epoch == 0:
+                return self.text_para_thresh, self.img_augment_thresh
+
+            # Exponent threshold base on Loss
+            k = (-1 / self.first_loss) * \
+                np.log(self.min_threshold / self.start_threshold)
+            k /= 3
+            updated_thresh = max(self.start_threshold * np.exp(-k *
+                                 (self.first_loss - self.current_loss)), self.min_threshold)
+
+        elif self.update_threshold_method == 'sigmoid_epoch':
+            # Sigmoid threshold base on Epoch
+            k = 2 * np.log((1 / self.min_threshold) - 1) / self.total_epochs
+            updated_thresh = max(self.start_threshold / (1 + np.exp(
+                k * (self.current_epoch - (self.total_epochs / 2)))), self.min_threshold)
+
+        else:
+            raise ValueError(
+                "Invalid update_threshold_method. Please choose 'linear', 'exponential_epoch', 'exponential_loss' or 'sigmoid_epoch'.")
+
+        return updated_thresh
+
+    def get_learning_rate(self):
+        if self.current_epoch == 0:
+            return self.init_learning_rate
+
+        # Linear learning rate decay
+        # decay = (self.init_learning_rate - self.init_learning_rate/10) * (self.current_epoch / self.total_epochs)
+        # updated_lr = max(self.init_learning_rate - decay, self.init_learning_rate / 10)
+
+        k = (-1 / self.first_loss) * \
+            np.log((self.init_learning_rate/10) / self.init_learning_rate)
+        k /= 3
+        updated_lr = max(self.init_learning_rate * np.exp(-k *
+                         (self.first_loss - self.current_loss)), self.min_threshold)
+
+        return updated_lr
+
     def forward(self, text_inputs, img_inputs):
-        text_thresh, img_thresh = self.get_threshold()
-        text_f = self.text_encoder(text_inputs, text_thresh)  # Encode text inputs
-        img_f = self.img_encoder(img_inputs, img_thresh)  # Encode image inputs
+        text_thresh = self.get_threshold()
+        text_f = self.text_encoder(
+            text_inputs, text_thresh)
+        img_f = self.img_encoder(img_inputs)
 
-        logits = self.classifier(text_f, img_f)  # Predict using classifier
+        logits = self.classifier(text_f, img_f)
 
         return logits
 
-    # Update the current epoch for dynamic threshold adjustment
     def update_epoch(self, epoch):
         self.current_epoch = epoch
+
+    def update_loss(self, loss):
+        if self.current_epoch == 0:
+            self.first_loss = loss
+        self.current_loss = loss
