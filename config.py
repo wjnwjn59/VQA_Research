89fa36b2df54682f0a465db4a19ab1b4c46af0e9
Koii2k3 <devkoi2k3@gmail.com>
2024-11-17 12:52:12 +0700

    Add filter arg

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class PipelineConfig:
         self.hidden_dim = 512
         self.projection_dim = 512
         self.weight_decay = 1e-5
-        self.patience = 5
+        self.patience = 3
         self.text_max_len = 50
         self.fusion_strategy = "concat+smalllen"
         self.text_encoder_id = "vinai/bartpho-word"
@@ -16,9 +16,10 @@ class PipelineConfig:
         self.paraphraser_id = "chieunq/vietnamese-sentence-paraphase"
         self.is_text_augment = True
         self.n_text_paras = 2
-        self.text_para_thresh = 0.5
+        self.text_para_thresh = 0.8
         self.n_text_para_pool = 30
         self.start_threshold = 0.6
+        self.is_filter = True
         self.use_dynamic_thresh = False
         self.dataset_name = 'vivqa'
         self.data_dir = "/home/VLAI/datasets"
